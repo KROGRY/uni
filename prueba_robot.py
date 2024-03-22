bfed656b6f1ab5e55cf955b8399771cf4a44db0b
XANDU <64363359+KROGRY@users.noreply.github.com>
2024-03-22 10:28:42 +0000

    Update prueba_robot.py

--- prueba_robot.py
+++ prueba_robot.py
@@ -23,15 +23,27 @@ class RoomNavigator:
         self.avoidance_direction = None  # "left" or "right"
 
         self.waypoints = [
+        (1.00, 6.2),  # Punto 1
             (2.00, 5.50),  # Punto 1
             (3.50, 4.50),  # Punto 2
             (3.50, 6.50),  # Punto 3
+            (3.50, 4.50),  # Punto 3
+            (6, 4.50),  # Punto 3
+            (6, 5.5),  # Punto 3
             (6.75, 5.50),  # Punto 4
+            (6.75, 6.50),  # Punto 4
+            (7.5, 6.50),  # Punto 4
+            (7.5, 1.50),  # Punto 4
             (6.75, 1.50),  # Punto 5
             (5.00, 1.50),  # Punto 6
             (5.00, 2.50),  # Punto 7
-            (2.50, 3.75),  # Punto 8
-            (2.50, 0.25),  # Punto 9
+            (4.2, 2.50),  # Punto 7
+            (4.2, 3.50),  # Punto 7
+            (2.50, 3.55),  # Punto 8
+            (4.2, 3.50),  # Punto 7
+            (4.2, 0.5),  # Punto 7
+            (2.50, 0.5),  # Punto 9
+            (0.5, 0.5),  # Punto 9
             (0.50, 4.50)   # Punto 10
         ]
 
@@ -74,22 +86,22 @@ class RoomNavigator:
 
         # State-based decision making
         if self.state == "avoiding_obstacle":
-            if self.obstacle_distance > 0.7:  # Considered safe distance
+            if self.obstacle_distance > 0.4:  # Considered safe distance
                 self.state = "navigating"  # Switch back to navigating state
             else:
                 twist.linear.x = 0.0
-                twist.angular.z = 0.5 if self.avoidance_direction == "left" else -0.5
+                twist.angular.z = 0.1 if self.avoidance_direction == "left" else -0.1
         elif self.state == "navigating":
-            if self.obstacle_distance < 0.5:  # Obstacle too close
+            if self.obstacle_distance < 0.3:  # Obstacle too close
                 self.state = "avoiding_obstacle"
                 self.avoidance_direction = "right" if self.left_side_obstacle_distance < self.right_side_obstacle_distance else "left"
                 twist.linear.x = 0.0
-                twist.angular.z = 0.5 if self.avoidance_direction == "left" else -0.5
+                twist.angular.z = 0.1 if self.avoidance_direction == "left" else -0.1
             elif distance_to_target > 0.2:
                 if abs(angle_diff) > 0.1:
                     twist.angular.z = 0.3 if angle_diff > 0 else -0.3
                 else:
-                    twist.linear.x = 0.5
+                    twist.linear.x = 0.2
             else:
                 self.current_waypoint_index += 1
                 print("Waypoint reached, moving to next.")
@@ -101,6 +113,7 @@ class RoomNavigator:
     def run(self):
         while not rospy.is_shutdown():
             self.navigate_to_waypoint()
+            print(self.state)
             self.rate.sleep()
 
 if __name__ == '__main__':
